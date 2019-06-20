8419c65a5e8eb84a11b654bf9c0334d598f1c6ee
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-20 16:57:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog
 
-  def name=(dog_name)
-    @this_dog_name = dog_name # Method takes in the argement of dog name and assigns it to an instance variable
+  def name=(dogs_name)
+    @this_dog_name = dogs_name # Method takes in the argement of dog name and assigns it to an instance variable
   end
 
   def name
@@ -13,6 +13,22 @@ class Dog
     puts "woof!" # Makes the dog bark
   end
 end
+
+# class Dog
+#
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
+#
+#   def name
+#     @this_dogs_name
+#   end
+# end
+#
+# lassie = Dog.new
+# lassie.name = "Lassie"
+#
+# puts lassie.name
 
 xena = Dog.new
 xena.name = "Xena"
